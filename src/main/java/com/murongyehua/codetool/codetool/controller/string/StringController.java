8f9a77a187a2ca94e5e34421e8a4391c9e2115e8
murongyehua <11371564@qq.com>
2019-09-03 08:53:28 +0800

    string dev: uuid finish

--- src/main/java/com/murongyehua/codetool/codetool/controller/string/StringController.java
+++ src/main/java/com/murongyehua/codetool/codetool/controller/string/StringController.java
@@ -1,5 +1,9 @@
 package com.murongyehua.codetool.codetool.controller.string;
 
+import com.murongyehua.codetool.codetool.dto.ResultMap;
+import com.murongyehua.codetool.codetool.service.string.StringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/string")
 public class StringController {
 
+    @Autowired
+    private StringService stringService;
 
+    @PostMapping("/normalUUID")
+    public ResultMap getNormalUUID(){
+        return stringService.normalUUID();
+    }
+
+    @PostMapping("/simpleUUID")
+    public ResultMap getSimpleUUID(){
+        return stringService.simpleUUID();
+    }
 
 }
